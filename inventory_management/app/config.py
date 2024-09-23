dcd33419f233e666a07fba0d53003fc4ba85b344
SRI VARSHINI REDDY <118330169+s223001489@users.noreply.github.com>
2024-09-23 19:37:59 +1000

    Update config.py

--- inventory_management/app/config.py
+++ inventory_management/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://sit722_part_3_6c9v_user:PIt83yDKEGX6IaNxbzIqersdKrIwmcCy@dpg-crm1t4o8fa8c73a05sh0-a.oregon-postgres.render.com/sit722_part_3_6c9v')
